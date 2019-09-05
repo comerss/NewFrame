3931be6b8a9e0fa7d933e44cbd8dd6ef81b4d696
叶庆华 <yeqinghua@donews.com>
2019-09-05 18:12:04 +0800

    增加了 字节码注入技术 demo

---
+++ buildSrc/src/main/java/com/comers/buildsrc/JavassitHelper.java
@@ -0,0 +1,10 @@
+package com.comers.buildsrc;
+
+import javassist.ClassPool;
+
+public class JavassitHelper  {
+    public static void  main(String args[]){
+        ClassPool pool=ClassPool.getDefault();
+
+    }
+}
